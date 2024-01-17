778d40e31a795bc62b904865d805011d79eb8463
FIYINFOLUWA <76849022+alatise001@users.noreply.github.com>
2024-01-17 01:49:49 +0100

    Changes made

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,68 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// import styles from './page.module.css'
+
+import Link from 'next/link'
+
+//home events contacts about spotify playlist
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <div className="background">
+      <header>
+        <h1 className="header-title" >DAVID</h1>
+        <h2 className="sub-title">NATHAN ILE</h2>
+      </header>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+      <main id="main">
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+        <div className="info-box" id="home">
+          <Link href={"/home"}>
+            <h3> HOME </h3>
+          </Link>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            the amazing story of redeemption of one of africa top gospel, musicians
           </p>
-        </a>
-      </div>
-    </main>
+        </div>
+
+        <div className="info-box text-right" id="events">
+          <Link href={"/events"}>
+            <h3> EVENTS </h3>
+          </Link>
+          <p>
+            the amazing story of redeemption of one of africa top gospel, musicians
+          </p>
+        </div>
+
+        <div className="info-box" id="about">
+          <Link href={"/about"}>
+            <h3> ABOUT</h3>
+          </Link>
+          <p>
+            the amazing story of redeemption of one of africa top gospel, musicians
+          </p>
+        </div>
+
+        <div className="info-box text-right" id="contacts">
+          <Link href={"/contacts"}>
+            <h3> CONTACTS</h3>
+          </Link>
+          <p>
+            the amazing story of redeemption of one of africa top gospel, musicians
+          </p>
+        </div>
+
+        <div className="info-box text-right" id="playlist">
+          <Link href={"/playlist"}>
+            <h3> PLAYLIST </h3>
+          </Link>
+          <p>
+            the amazing story of redeemption of one of africa top gospel, musicians
+          </p>
+        </div>
+
+      </main>
+
+    </div>
   )
 }
